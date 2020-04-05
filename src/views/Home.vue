7c43f9d1538e1b2f75b6461fa21c4972266fe099
kayyismka <kayyismka09@gmail.com>
2020-04-05 13:20:27 +0700

    upload naoki-store

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderNaoki />
+    <CarouselNaoki />
+    <WomanNaoki />
+    <InstaNaoki />
+    <PartnerLogoNaoki />
+    <FooterNaoki />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderNaoki from "@/components/HeaderNaoki.vue";
+import CarouselNaoki from "@/components/CarouselNaoki.vue";
+import WomanNaoki from "@/components/WomanNaoki.vue";
+import InstaNaoki from "@/components/InstaNaoki.vue";
+import PartnerLogoNaoki from "@/components/PartnerLogoNaoki.vue";
+import FooterNaoki from "@/components/FooterNaoki.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderNaoki,
+    CarouselNaoki,
+    WomanNaoki,
+    InstaNaoki,
+    PartnerLogoNaoki,
+    FooterNaoki
+  }
+};
+</script>
